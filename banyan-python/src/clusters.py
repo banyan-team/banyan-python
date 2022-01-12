a22780dc3de056be8e0027ab2288c4bbda81384b
chloewin <chloewin@uw.edu>
2022-01-11 22:47:17 -0800

    Clean clusters.py

--- banyan-python/src/clusters.py
+++ banyan-python/src/clusters.py
@@ -1,10 +1,16 @@
 from _typeshed import NoneType
-import boto3
-import random
+import logging
 import time
-from typing import Dict, Any, Union
+import boto3
+from typing import Dict, Any, Optional, Union
 import warnings
+from config import configure
+from utils import send_request_get_response
+from pathlib import Path
+import os
 
+s3 = boto3.client('s3')
+clusters = Dict()
 
 def create_cluster(
     name:Union[str,NoneType]=None,
@@ -16,29 +22,39 @@ def create_cluster(
     s3_bucket_arn:Union[str,NoneType]=None,
     s3_bucket_name:Union[str,NoneType]=None,
     scaledown_time=25,
+    region=None,
     vpc_id=None,
     subnet_id=None,
     nowait=False,
     **kwargs,
 ):
-    clusters = get_clusters(**kwargs) # TODO: where does this function come from?
+    global clusters
+
+    # Configure using parameters
+    c = configure(**kwargs)
+
+    clusters_local = get_clusters(**kwargs)
     if name is None:
-        name = "Cluster " + str(len(clusters) + 1)
+        name = "Cluster " + str(len(clusters_local) + 1)
+    if region is None:
+        region = get_aws_config_region() 
+        # TODO: how to get aws config retion
 
     # Check if the configuration for this cluster name already exists
     # If it does, then recreate cluster
-    if name in clusters:
-        if clusters[name].status == "terminated":
-            warnings.warn(f"Cluster configuration with {name} already exists. Ignoring new configuration and re-creating cluster.") # TODO: confirm this is what we want
-            send_request_get_response("create_cluster", {"cluster_name": name, "recreate":True})
-            if not nowait: # TODO: where does this come from?
+    if name in clusters_local:
+        if clusters_local[name].status == "terminated":
+            logging.info(f"Started re-creating cluster named {name}")
+            send_request_get_response(
+                "create_cluster",
+                Dict(cluster_name=name, recreate=True)
+            )
+            if not nowait:
                 wait_for_cluster(name)
             return get_cluster(name)
         else:
-            raise RuntimeError(f"Cluster with {name} already exists and has status {str(clusters[name].status)}") # TODO: what kind of error should be raised?
-        
-    # Construct arguments
-
+            logging.error(f"Cluster with name {name} already exists and its current status is {str(clusters_local[name].status)}") 
+    # TODO: Confirm logging.error, logging.info is what we want to use
     # Construct arguments
     if s3_bucket_name is not None:
         s3_bucket_arn = f"arn:aws:s3:::{s3_bucket_name}"
@@ -47,21 +63,21 @@ def create_cluster(
 
     if s3_bucket_arn is None:
         s3_bucket_arn = ""
-    elif s3_bucket_name not in s3_list_buckets(get_aws_config()):
-        raise ValueError(f"Bucket {s3_bucket_name} does not exist in connected AWS account") # TODO: is this correct type of error
+    elif s3_bucket_name not in s3.list_buckets():#s3_list_buckets(get_aws_config()): # TODO any need to pass config? I don't see a way to do so
+        logging.error(f"Bucket {s3_bucket_name} does not exist in connected AWS account") 
 
     # Construct cluster creation
-    cluster_config = {
-        "cluster_name": name,
-        "instance_type": instance_type,
-        "max_num_workers": max_num_workers,
-        "initial_num_workers": initial_num_workers,
-        "min_num_workers": min_num_workers,
-        "aws_region": get_aws_config_region(),
-        "s3_read_write_resource": s3_bucket_arn,
-        "scaledown_time": scaledown_time,
-        "recreate": False,
-    }
+    cluster_config = Dict(
+        cluster_name = name,
+        instance_type = instance_type,
+        max_num_workers = max_num_workers,
+        initial_num_workers = initial_num_workers,
+        min_num_workers = min_num_workers,
+        aws_region = region, # TODO
+        s3_read_write_resource = s3_bucket_arn,
+        scaledown_time = scaledown_time,
+        recreate = False,
+    )
 
     if "ec2_key_pair_name" in c["aws"]:
         cluster_config["ec2_key_pair"] = c["aws"]["ec2_key_pair_name"]
@@ -72,36 +88,42 @@ def create_cluster(
     if subnet_id is not None:
         cluster_config["subnet_id"] = subnet_id
 
-    print("Creating cluster")
+    logging.info(f"Started creating cluster named {name}")
 
     # Send request to create cluster
     send_request_get_response("create_cluster", cluster_config)
 
     if not nowait:
         wait_for_cluster(name)
-    
-    return Cluster(name, get_cluster_status(name), "", 0, s3_bucket_arn)
+
+    cl = Cluster(name, get_cluster_status(name), "", 0, s3_bucket_arn)
+    clusters[name] = cl
+
+    return cl
 
     
 def destroy_cluster(name: str, **kwargs):
     configure(**kwargs)
-    send_request_get_response("destroy_cluster", Dict[str, Any]({"cluster_name": name}))
+    logging.info(f"Destroying cluster named {name}")
+    send_request_get_response("destroy_cluster", Dict[str, Any](cluster_name = name))
 
 
 def delete_cluster(name: str, **kwargs):
     configure(**kwargs)
-    send_request_get_response("destroy_cluster", Dict[str, Any]({"cluster_name": name, "permanently_delete": True}))
+    logging.info(f"Deleting cluster named {name}")
+    send_request_get_response("destroy_cluster", Dict[str, Any](cluster_name = name, permanently_delete = True))
 
 
 def update_cluster(name: str, **kwargs):
     configure(**kwargs)
-    send_request_get_response("update_cluster", Dict[str, Any]({"cluster_name": name}))
+    logging.info(f"Updating cluster named {name}")
+    send_request_get_response("update_cluster", Dict[str, Any](cluster_name = name))
 
 
 def assert_cluster_is_ready(name: str, **kwargs):
-    print("Setting cluster status to running") # TODO: desired?
+    logging.info(f"Setting status of cluster named {name} to running") 
     configure(**kwargs)
-    send_request_get_response("set_cluster_ready", Dict[str, Any]({"cluster_name": name}))
+    send_request_get_response("set_cluster_ready", Dict[str, Any](cluster_name = name))
 
 
 class Cluster:
@@ -112,7 +134,7 @@ class Cluster:
         self.num_jobs_running = num_jobs_running
         self.s3_bucket_arn = s3_bucket_arn
 
-def parsestatus(status):
+def parsestatus(status: str):
     accepted = ["creating", "destroying", "updating", "failed", "starting",
     "stopped", "running", "terminated"]
     if status in accepted:
@@ -121,48 +143,66 @@ def parsestatus(status):
         raise ValueError(f"Unexpected status {status}")
 
 
-def get_clusters(**kwargs):
+def get_clusters(cluster_name=None, **kwargs):
+    logging.debug("Downloading description of clusters")
     configure(**kwargs)
-    response = send_request_get_response("describe_clusters", Dict[str, Any]({}))
-    return {
-        name: Cluster(
-            name,
-            parsestatus(c["status"]),
-            c["status_explanation"] if "status_explanation" in c else "",
-            c["num_jobs"],
-            c["s3_read_write_resource"],
-        ) for (name, c) in response["clusters"]
-    }
+    filters = Dict()
+    if cluster_name is not None:
+        filters["cluster_name"] = cluster_name
+    response = send_request_get_response("describe_clusters", Dict[str, Any](filters=filters))
+    clusters_dict = Dict(
+        {
+            name:Cluster(
+                name,
+                parsestatus(c["status"]),
+                c["status_explanation"] if "status_explanation" in c else "",
+                c["num_jobs"],
+                c["s3_read_write_resource"],
+            ) for (name, c) in response["clusters"]
+        }
+    )
+
+    # Cache info
+    global clusters
+    for (name, c) in clusters_dict:
+        clusters[name] = c
+    
+    return clusters_dict
+
+
+def get_cluster_s3_bucket_arn(cluster_name=None, **kwargs):
+    if cluster_name is None:
+        cluster_name = get_cluster_name()
+    configure(**kwargs)
+    global clusters
+    # Check if cached, since this property is immutable
+    if cluster_name not in clusters:
+        get_cluster(cluster_name)
+    return clusters[cluster_name].s3_bucket_arn
 
 
 def get_cluster_s3_bucket_name(cluster_name=None, **kwargs):
     if cluster_name is None:
         cluster_name = get_cluster_name()
     configure(**kwargs)
-    cluster = get_cluster(cluster_name)
-    return s3_bucket_arn_to_name(cluster.s3_bucket_arn)
+    return s3_bucket_arn_to_name(get_cluster_s3_bucket_arn(cluster_name))
 
 
-def get_cluster(name: str=None, **kwargs):
+def get_cluster(name: Optional[str]=None, **kwargs):
     if name is None:
         name = get_cluster_name()
-        return get_clusters(**kwargs)[name]
-
-
-def get_running_clusters(*args, **kwargs):
-    return [entry for entry in get_clusters(*args, **kwargs) if entry[2].status == "running"]
-
-
-def get_cluster_status(name:str=None, **kwargs):
-    if name is None:
-        name = get_cluster_name()
-    c = get_clusters(**kwargs)[name]
-    if c.status == "failed":
-        print(c.status_explanation) # TODO: OK?
+    global clusters
+    if name in clusters:
+        if clusters[name].status is "failed":
+            logging.error(c.status_explanation)
+    c = get_clusters(name, **kwargs)
+    if c.status is "failed":
+        logging.error(c.status_explanation)
     return c.status
 
 
 def wait_for_cluster(name:str=None, **kwargs):
+    # TODO: may need to be updated
     if name is None:
         name = get_cluster_name()
     t = 5
@@ -182,3 +222,51 @@ def wait_for_cluster(name:str=None, **kwargs):
         print(f"Cluster {name} no longer exists")
     elif cluster_status not in ["creating", "updating"]:
         print(f"Cluster {name} setup has failed")
+
+def upload_to_s3(src_path, dst_name=None, cluster_name=None, **kwargs):
+    if dst_name is None:
+        dst_name = os.path.basename(src_path)
+    if cluster_name is None:
+        cluster_name = get_cluster_name()
+    configure(**kwargs)
+    bucket_name = get_cluster_s3_bucket_name(cluster_name)
+
+    configure(**kwargs)
+
+    if src_path.startswith("http://") or src_path.startswith("https://"):
+        with urllib.request.urlopen(src_path) as f:
+            s3.upload_fileobj(f, bucket_name, dst_name)
+    if src_path.startswith("s://"):
+        bucket_src, key_src = src_path.replace("s3://", "").split("/", 1)
+        copy_source = {
+            'Bucket': bucket_src,
+            'Key': key_src
+        }
+        s3.meta_client.copy(copy_source, bucket_name, dst_name)
+    else:
+        if src_path.startswith("file://"):
+            src_path = src_path[8:]
+        src_path = Path(src_path)
+        
+        if src_path.is_file():
+            s3.meta.client.upload_file(src_path, bucket_name, dst_name)
+        else:
+            files = [f for f in os.listdir(src_path) if os.path.isfile(os.path.join(src_path, f))]
+            for file in files:
+                s3.meta.client.upload_file(Path(os.path.join(src_path, file)), bucket_name, os.path.join(dst_name, file))
+    return dst_name
+
+# ####
+# def get_running_clusters(*args, **kwargs):
+#     return [entry for entry in get_clusters(*args, **kwargs) if entry[2].status == "running"]
+
+
+def get_cluster_status(name:str=None, **kwargs):
+    if name is None:
+        name = get_cluster_name()
+    c = get_clusters(name, **kwargs)[name]
+    if c.status == "failed":
+        print(c.status_explanation) # TODO: OK?
+    return c.status
+
+
--- banyan-python/src/utils.py
+++ banyan-python/src/utils.py
@@ -1,11 +1,12 @@
+import os
 from typing import Dict
 import requests
 from config import load_config
+import logging
 
-
-def method_to_string(method) = begin
+def method_to_string(method):
     if str(method).replace("_", "-") in ["create-cluster","destroy-cluster","describe-clusters",
-    "create-job","destroy-job","describe-jobs","evaluate","update-cluster","set-cluster-ready"):
+    "create-job","destroy-job","describe-jobs","evaluate","update-cluster","set-cluster-ready"]:
         return str(method)
 
 
@@ -14,8 +15,12 @@ def send_request_get_response(method: str, content:dict):
     configuration = load_config()
     user_id = configuration["banyan"]["user_id"]
     api_key = configuration["banyan"]["api_key"]
-    url = str(BANYAN_API_ENDPOINT, method_to_string(method))
-    content["debug"] = is_debug_on()
+    if os.get_env("BANYAN_API_ENDPOINT", default=None) is None:
+        banyan_api_endpoint = "https://4whje7txc2.execute-api.us-west-2.amazonaws.com/prod/"
+    else:
+        banyan_api_endpoint = os.get_env("BANYAN_API_ENDPOINT", default=None)
+    url = str(banyan_api_endpoint, method_to_string(method))
+    # content["debug"] = is_debug_on()
     headers = {
         "content-type": "application/json",
         "Username-APIKey": f"{user_id}-{api_key}"
@@ -33,6 +38,9 @@ def send_request_get_response(method: str, content:dict):
         return None
     elif resp.status_code == 500 or resp.status_code == 504:
         raise Exception(data)
-    elif resp.status == 502
+    elif resp.status == 502:
         raise Exception("Sorry there has been an error. Please contact support.")
     return data
+
+def is_debug_on():
+    return logging.DEBUG >= logging.root.level
