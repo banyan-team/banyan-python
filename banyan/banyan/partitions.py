8cf7fa622175de687a8b948f230a616f02a59234
cleahwin <cleahwin@gmail.com>
2022-07-14 10:27:51 -0700

    Complete partitions.py

--- banyan/banyan/partitions.py
+++ banyan/banyan/partitions.py
@@ -1,11 +1,17 @@
 from types import NoneType
 from future import Future
-from typing import Union
+from typing import Union, List
 from copy import copy
 
 from plum import dispatch
 
-from utils_partitions import PartitioningConstraintOverGroup
+from utils_partitions import (
+    PartitioningConstraintOverGroup, 
+    PartitionType, 
+    PartitioningConstraints, 
+    PartitioningConstraint,
+    PartitionedUsingFunc
+)
 
 def pt_ref_to_jl (f: Future):
     return (f.value_id, 0)
@@ -21,7 +27,7 @@ def Match (args: list):
 
 def MatchOn (on:str, args: list):
     return PartitioningConstraintOverGroup(
-        "MATCH_ON=" * string(on),
+        "MATCH_ON=" * str(on),
         pt_refs_to_jl(args),
     )
 
@@ -37,14 +43,13 @@ def Scale(
     to: float, 
     by: float, 
     relative_to: list
-)
+):
     new_relative_to: list = Future[arg]
-    new_relative_to.extend(copy(relative_to)
+    new_relative_to.extend(copy(relative_to))
     PartitioningConstraintOverGroup(
         to = "SCALE_BY=$by" if to < 0.0 else "SCALE_TO=$to",
         pt_refs_to_jl(new_relative_to)
     )
-)
 
 @dispatch
 def Scale (
@@ -54,3 +59,66 @@ def Scale (
     relative_to: list = Future
 ):
     Scale(arg, -1.0 if (to is None) else parse_bytes(to), convert(float, by):float, relative_to)
+
+PTOrPTUnion = Union[PartitionType, List[PartitionType]]
+
+def merge_pts (a: PartitionType, b: PartitionType, pts_so_far: List[PartitionType]):
+    all_params_matching = True
+
+    for param_name in keys(a.parameters):
+        if b.parameters.haskey(param_name):
+            all_params_matching = all_params_matching and a.parameters[param_name] == b.parameters[param_name]
+        if not all_params_matching:
+            break
+
+    new_constraints = []
+
+    for c in a.constraints.constraints:
+        new_constraints.append(c)
+    for c in b.constraints.constraints:
+        new_constraints.append(c)
+
+    if all_params_matching:
+        pts_so_far.append(PartitionType(merge(a.parameters, b.parameters), PartitioningConstraints(new_constraints)))
+
+def And (a: PTOrPTUnion, b: PTOrPTUnion):
+    if isinstance(a, PartitionType) and isinstance(b, PartitionType):
+        res = []
+        for a_pt in a:
+            merge_pts(a_pt, b, res)
+        return res
+    elif isinstance(a, List[PartitionType]) and isinstance(b, PartitionType):
+        res = []
+        for a_pt in a:
+            merge_pts(a_pt, b, res)
+        return res
+    elif isinstance(a, List[PartitionType]) and isinstance(b, List[PartitionType]):
+        res = []
+        for a_pt in a:
+            for b_pt in b:
+                merge_pts(a_pt, b_pt, res)
+        return res
+    elif isinstance(a, PartitionType) and isinstance(b, List[PartitionType]):
+        return b and a
+
+def Or(a: PTOrPTUnion, b: PartitionType):
+    return a + b
+
+def Partitions():
+    Partitions(dict{ValueId,PartitionTypeComposition}())
+
+def PartitionAnnotation(): 
+    PartitionAnnotation(Partitions(), PartitioningConstraints())
+
+NOTHING_PARTITIONED_USING_FUNC = PartitionedUsingFunc(
+    False,
+    Future,
+    False,
+    int,
+    tuple{Future,List[int]}[],
+    False,
+    False,
+    True
+)
+
+
--- banyan/banyan/utils_partitions.py
+++ banyan/banyan/utils_partitions.py
@@ -1,7 +1,10 @@
 from typing import Any, Dict, List, Tuple
 
+
 from future import Future
 from id import ValueId
+from id import generate_bang_value
+
 
 PartitionTypeReference = Tuple[ValueId, int]
 
@@ -29,12 +32,20 @@ class PartitioningConstraints:
     def __init__ (self, constraints: List[PartitioningConstraint]):
         self.constraints = constraints
 
+    def to_py (self):
+        return {
+            "constraints": [
+                constraint.to_py()
+                for constraint in self.constraints
+            ]
+        }
+
 PartitionTypeParameters = Dict[str, Any]
 
 class PartitionType:
     def __init__(self, *args):
         if (len(args) == 2) and isinstance(args[0], PartitionTypeParameters) and isinstance(args[1], PartitioningConstraints):
-            self.parameters = args[0]
+            self.  = args[0]
             self.constraints = args[1]
         else:
             args = args[0]
@@ -50,25 +61,44 @@ class PartitionType:
                 elif isinstance(arg, PartitioningConstraint):
                     arg.append(constraints.constraints)
                 # TODO - figure out func
-                elif isinstance(arg, func):
+                elif isinstance(callable(arg)):
                     PartitioningConstraintFunction(arg).append(constraints.constraints)
                 else:
                     raise Exception("Expected either a partition type parameter or constraint")
                 self.parameters = parameters
+
+    def to_py(self):
+        for (k, v) in self.parameters:
+            if v == "!":
+                self.parameters[k] = generate_bang_value()
+        return {"parameters": self.parameters, "constraints": to_py(self.constraints)}
+        
+
 
 
 class PartitionTypeComposition:
     def __init__ (self,  pts: List[PartitionType]):
         self.pts = pts
 
+    def to_py (self):
+        map(lambda p: p.to_py(), self.pts)
+
 class Partitions:
     def __init__ (self, pt_stacks: Dict[ValueId, PartitionTypeComposition]):
         self.pt_stacks = pt_stacks
+
+    def to_py (self):
+        return ("pt_stacks": (v: to_py(self.pt_stacks) for (v, self.ptc) in p.pt_stacks),
+    )
+
 
 class PartitionAnnotation:
     def __init__ (self, partitions: Partitions, constraints, PartitioningConstraints):
         self.partitions = partitions
         self.constraints = constraints
+
+    def to_py (self):
+        return {"partitions": to_py(self.partitions), "constraints": to_py(self.constraints)}
 
 class PartitionedUsingFunc:
     def __init__ (self, grouped: List[Future], keep_same_keys: bool, keys: list, keys_by_future: List[Tuple[Future, list]], renamed: bool, drifted: bool, isnothing: bool):
@@ -79,9 +109,11 @@ class PartitionedUsingFunc:
         self.renamed = renamed
         self.drifted = drifted
         self.isnothing = isnothing
+    
+    def is_none(self):
+        f.is_none;
 
 class PartionedWithFunc:
     def __init__ (self, func, future_):
         self.func = func
         self.future_ = future_
-
