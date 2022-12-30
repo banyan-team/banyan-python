656eaa7a3bb1b020ac1dab8d1c531c630064b763
Caleb Winston <calebhwin@gmail.com>
2022-12-30 00:30:51 -0800

    Remove unnecessary PF dispatch table file

---
+++ banyan/banyan/utils_communication.py
@@ -0,0 +1,10 @@
+from typing import Any
+
+# TODO: Store gather/scatter/shuffle queues
+
+
+def send_to_client(data: Any):
+    raise NotImplementedError()
+
+def receive_to_client():
+    raise NotImplementedError()
